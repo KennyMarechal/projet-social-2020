6a4122ba4b03a1a1bafbcac2779d0fcdd40795dc
Hyvong <Emustle@users.noreply.github.com>
2020-12-06 23:08:18 -0500

    Dimensions vue 1280x720
    Les dimensions des vues ont été modifiées pour remplir un espace de 1280x720

--- ProjetSocial/src/vue/Fenetre.java
+++ ProjetSocial/src/vue/Fenetre.java
@@ -8,7 +8,7 @@ public class Fenetre extends Navigateur {
 	@Override
 	public void start(Stage stade) throws Exception {
 		stade.setWidth(1280);
-		stade.setHeight(720);
+		stade.setHeight(750);
 		stade.setTitle("Titre");
 		stade.setScene(Controleur.selectionnerVuePrincipale()); // une vue est appliquer a la fenetre
 		stade.show();
