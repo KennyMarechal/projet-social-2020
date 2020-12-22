44ccce8963fd1458ef0df2ea37293592b59717f6
Max <mdaiglevigneault@gmail.com>
2020-12-22 15:40:05 -0500

    optimisation Statistique SQL

--- Base de donnée/social.sql
+++ Base de donnée/social.sql
@@ -341,3 +341,63 @@ CREATE TRIGGER set_timestamp BEFORE UPDATE ON public.message FOR EACH ROW EXECUT
 -- PostgreSQL database dump complete
 --
 
+---
+---Ajouts Manuel
+---
+
+---
+---Fonction de recuperation de tout les usagers contenu dans les salons de l'usager spécifié
+---
+
+CREATE OR REPLACE FUNCTION listerContactsDeUtilisateurID(utilisateurID int)
+  RETURNS TABLE (utilisateur_id   int 
+               , salon_id   int) AS
+$func$
+BEGIN
+	RETURN QUERY
+   
+	SELECT autorisation.utilisateur_id, autorisation.salon_id
+	From autorisation
+	INNER JOIN (SELECT autorisation.salon_id from autorisation where autorisation.utilisateur_id = utilisateurID) listeAutorisationsUtilisateur
+	ON autorisation.salon_id = listeAutorisationsUtilisateur.salon_id;
+   
+END
+$func$  LANGUAGE plpgsql;
+
+---
+---Fonction de génération d'une grille de statistique des usagers contenu dans les salons de l'usager spécifié
+---
+
+CREATE OR REPLACE FUNCTION genererStatistiqueDeUtilisateurID(utilisateurID int)
+  RETURNS TABLE (pseudo character varying (30)
+	  			, utilisateur_id   int
+				, heure double precision
+				, frequence bigint
+				, heureminimale double precision) AS
+$func$
+DECLARE
+	maintenant timestamp with time zone := NOW();
+BEGIN
+	RETURN QUERY
+   
+	SELECT  source.pseudo, 
+			source.utilisateur_id, 
+			source.heure, 
+			count(source.heure) as frequence, 
+			(SELECT date_part('hour', maintenant - time '13:00') as heureminimale)
+	FROM
+	(SELECT	
+		(SELECT utilisateurs.pseudo from utilisateurs where utilisateurs.id = message.utilisateur_id),
+		message.utilisateur_id,
+		date_part('hour', message.moment) as heure
+		From message
+		INNER JOIN (Select listerContactsDeUtilisateurID.utilisateur_id, listerContactsDeUtilisateurID.salon_id from listerContactsDeUtilisateurID(1)) listeContactsUtilisateur
+		ON message.utilisateur_id = listeContactsUtilisateur.utilisateur_id AND 
+		message.salon_id = listeContactsUtilisateur.salon_id AND 
+		age(date_trunc('hour', maintenant), message.moment) <= time '13:00'
+
+	) source
+	GROUP BY source.pseudo, source.utilisateur_id, source.heure
+	ORDER BY source.utilisateur_id;
+END
+$func$  LANGUAGE plpgsql;
---
+++ Fichier PHP/Maxime/apres Optimisation/accesseur/StatistiqueDAO.php
@@ -0,0 +1,30 @@
+<?php
+    include_once "BasedeDonnees.php";
+	
+	class StatistiqueDAO
+	{
+		function getGraphiqueStatistiqueParClient($idClient)
+		{
+			$LISTERSTATISTIQUES = "SELECT pseudo, utilisateur_id, heure, frequence, heureminimale
+			From genererStatistiqueDeUtilisateurID($idClient);";
+			$requete = BaseDeDonnees::getConnection()->prepare($LISTERSTATISTIQUES);
+			$requete->bindParam(':idClient', $idClient, PDO::PARAM_INT);
+			$requete->execute();
+			$listeStatistiqueGraphique = $requete->fetchAll();
+			return $listeStatistiqueGraphique;
+		}
+		
+		function getStatMessageParClient($idClient)
+		{
+			$STATISTIQUEMESSAGE = "SELECT Count(id) 
+			From message 
+			Where utilisateur_id = :idClient AND 
+			age(date_trunc('hour', NOW()), message.moment) <= time '13:00';";
+			$requete = BaseDeDonnees::getConnection()->prepare($STATISTIQUEMESSAGE);
+			$requete->bindParam(':idClient', $idClient, PDO::PARAM_INT);
+			$requete->execute();
+			$statistiqueMessage = $requete->fetchAll();
+			return $statistiqueMessage;
+		}
+	}
+?>
